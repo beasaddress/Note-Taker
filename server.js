aa8bf570d0813b15d4a32acaa0216bffb882af22
Beatriz Sandoval <beasaddress@gmail.com>
2023-06-17 20:08:54 -0700

    initializing express app and created a port

--- server.js
+++ server.js
@@ -2,3 +2,8 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+
+//initializing express app and creating a port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
