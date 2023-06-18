e2c5d10936306a7a603351923f5045c8bf213451
Beatriz Sandoval <beasaddress@gmail.com>
2023-06-17 21:28:37 -0700

    set up a route handler for get requests to notes path

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,14 @@
-
+const router = require('express').Router();
+const store = require('../db/store');
+//setting up a route handler for a get request to /notes path
+router.get('/notes', (req, res) => {
+    //this get request will call a function from the store module
+    store.getNotes()
+    .then((notes) => {
+        return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+});
 
 
 module.exports = router;
