b3744059d37f8d8fcd88dfde5c3479093e6e6d6b
Beatriz Sandoval <beasaddress@gmail.com>
2023-06-17 21:54:46 -0700

    added a route to handle delete requests that will send a json response confirming the note was removed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -16,7 +16,13 @@ router.post('/notes', (req, res) => {
     .then((note) => res.json(note))
     .catch((err) => res.status(500).json(err));
 });
-
-
+//setting up a route to handle deleting notes. the delete request
+//will call a removeNote funtion that will pass the note id as an arguement to remove it from the database
+router.delete('/notes/:id', (req, res) => {
+    store.removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))//the callback function will send a json response indicating
+    //the note was removed. response body will contain ok true
+    .catch((err) => res.status(500).json(err));
+});
 
 module.exports = router;
